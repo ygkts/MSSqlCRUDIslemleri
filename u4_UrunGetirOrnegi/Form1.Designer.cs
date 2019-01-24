e2240846ca87150779ac148c125844b0162f75c9
Yasemin Göktaş <ygkts61@gamil.com>
2019-01-24 18:33:09 +0300

    first

---
+++ u4_UrunGetirOrnegi/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace u4_UrunGetirOrnegi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUrunGetir = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.buttonEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtStok = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.txtBulID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonSil = new System.Windows.Forms.Button();
+            this.buttonGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonUrunGetir
+            // 
+            this.buttonUrunGetir.Location = new System.Drawing.Point(25, 54);
+            this.buttonUrunGetir.Name = "buttonUrunGetir";
+            this.buttonUrunGetir.Size = new System.Drawing.Size(69, 61);
+            this.buttonUrunGetir.TabIndex = 0;
+            this.buttonUrunGetir.Text = "ÜRÜN GETİR";
+            this.buttonUrunGetir.UseVisualStyleBackColor = true;
+            this.buttonUrunGetir.Click += new System.EventHandler(this.buttonUrunGetir_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "( ID ) ÜRÜN GETİR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonEkle
+            // 
+            this.buttonEkle.Location = new System.Drawing.Point(740, 58);
+            this.buttonEkle.Name = "buttonEkle";
+            this.buttonEkle.Size = new System.Drawing.Size(76, 61);
+            this.buttonEkle.TabIndex = 0;
+            this.buttonEkle.Text = "EKLE";
+            this.buttonEkle.UseVisualStyleBackColor = true;
+            this.buttonEkle.Click += new System.EventHandler(this.buttonEkle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(952, 475);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(305, 63);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(100, 22);
+            this.txtAd.TabIndex = 2;
+            // 
+            // txtStok
+            // 
+            this.txtStok.Location = new System.Drawing.Point(461, 63);
+            this.txtStok.Name = "txtStok";
+            this.txtStok.Size = new System.Drawing.Size(100, 22);
+            this.txtStok.TabIndex = 2;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(618, 65);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 2;
+            // 
+            // txtBulID
+            // 
+            this.txtBulID.Location = new System.Drawing.Point(112, 93);
+            this.txtBulID.Name = "txtBulID";
+            this.txtBulID.Size = new System.Drawing.Size(142, 22);
+            this.txtBulID.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(566, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fiyat :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(411, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Stok :";
+            // 
+            // buttonSil
+            // 
+            this.buttonSil.Location = new System.Drawing.Point(837, 58);
+            this.buttonSil.Name = "buttonSil";
+            this.buttonSil.Size = new System.Drawing.Size(140, 27);
+            this.buttonSil.TabIndex = 4;
+            this.buttonSil.Text = "SİL";
+            this.buttonSil.UseVisualStyleBackColor = true;
+            this.buttonSil.Click += new System.EventHandler(this.buttonSil_Click);
+            // 
+            // buttonGuncelle
+            // 
+            this.buttonGuncelle.Location = new System.Drawing.Point(837, 91);
+            this.buttonGuncelle.Name = "buttonGuncelle";
+            this.buttonGuncelle.Size = new System.Drawing.Size(140, 27);
+            this.buttonGuncelle.TabIndex = 4;
+            this.buttonGuncelle.Text = "GÜNCELLE";
+            this.buttonGuncelle.UseVisualStyleBackColor = true;
+            this.buttonGuncelle.Click += new System.EventHandler(this.buttonGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 644);
+            this.Controls.Add(this.buttonGuncelle);
+            this.Controls.Add(this.buttonSil);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBulID);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtStok);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonEkle);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonUrunGetir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUrunGetir;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonEkle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtStok;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.TextBox txtBulID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonSil;
+        private System.Windows.Forms.Button buttonGuncelle;
+    }
+}
+
---
+++ u4_UrunGetirOrnegi/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace u4_UrunGetirOrnegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string baglanti = "Server = YASEMINGOKTAS; Database = NORTHWND; Trusted_Connection=true;";
+        SqlConnection conn = new SqlConnection(baglanti);
+        SqlCommand cmd;
+        private void UrunGetir(string sorgu)
+        {
+            SqlDataAdapter adap = new SqlDataAdapter(sorgu, conn);
+            DataTable tablo = new DataTable();
+            adap.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }  
+        private void buttonUrunGetir_Click(object sender, EventArgs e)
+        {
+            string sorgu = "select * from Products";
+            UrunGetir(sorgu);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtBulID.Text != "")
+                {
+                    SqlCommand cmd = new SqlCommand("select * from Products where ProductID = @pID", conn);
+                    cmd.Parameters.AddWithValue("@pID", txtBulID.Text);
+
+                    SqlDataAdapter adap = new SqlDataAdapter(cmd);
+                    DataTable tablo = new DataTable();
+                    adap.Fill(tablo);
+
+                    dataGridView1.DataSource = tablo;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void buttonEkle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtAd.Text != "" )
+                {
+                    conn.Open();
+                    cmd = new SqlCommand("insert into Products (ProductName, UnitsInStock, UnitPrice) values (@ProductName, @UnitsInStock, @UnitPrice)",conn);
+                    cmd.Parameters.AddWithValue("@ProductName", txtAd.Text);
+                    cmd.Parameters.AddWithValue("@UnitsInStock", txtStok.Text);
+                    cmd.Parameters.AddWithValue("@UnitPrice", txtFiyat.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("ÜRÜN EKLENDİ..");
+
+
+                    UrunGetir("select * from products");
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        int id;
+        private void buttonGuncelle_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string updateSorgu = "update Products set ProductName = @name, UnitPrice = @uprice, UnitsInStock = @Stock where ProductID = @pID ";
+            cmd = new SqlCommand(updateSorgu,conn);
+            cmd.Parameters.AddWithValue("@pID", id);
+            cmd.Parameters.AddWithValue("@name", txtAd.Text);
+            cmd.Parameters.AddWithValue("@uprice", txtFiyat.Text);
+            cmd.Parameters.AddWithValue("@Stock", txtStok.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Güncellendi..");
+            UrunGetir("select * from products");
+            conn.Close();
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            txtAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtFiyat.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            txtStok.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+        }
+
+        private void buttonSil_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string deleteSorgu = "delete from Products where ProductID = @pID";
+            cmd = new SqlCommand(deleteSorgu, conn);
+            cmd.Parameters.AddWithValue("pID",id);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Silindi..");
+            UrunGetir("Select * from Products");
+            conn.Close();
+        }
+    }
+}
